ca0b5c45fab694b930114b28070563e75b13a2eb
roopak07 <h.rupak61@gmail.com>
2019-04-21 14:08:33 +0530

    Update lengh & begining node creation.c

--- lengh & begining node creation.c
+++ lengh & begining node creation.c
@@ -1,18 +1,30 @@
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
-int len();
-void begining();
 struct node
 {
 	int data;
 	struct node *link;
 };
-struct node *root;
+struct node *root; // gloabl variable 'root'
+int len();
+void begining();
+void append();
+void display();
+
 void main()
 {
+	  append();
+	  begining();
+	  int l=len();
+	  printf("lenght of the linked list:%d\n",l);
+	  display();
+	  getch();
+}
+
+void append()
+{
 	struct node *temp,*p;
-	int count=0;
 	root=NULL;
 	temp=(struct node*)malloc(sizeof(struct node));
 	printf("new node location is:%u\n",temp);
@@ -32,35 +44,24 @@ void main()
 		}
 		p->link=temp;
 	}
-	  begining();
-	int l=len();
-		printf("lenght of the linked list:%d\n",l);
-		printf("root:%d\n",root);
-		printf("nodes data:\n");
-		p=root;
-		while(p!=NULL)
-		{
-			count++;
-			printf("node %d data:%d link address:%u\n",count,p->data,p->link);
-			p=p->link;
-		}
-		
-		getch();
 }
 
 void begining()
 {
-	struct node *temp;
+	struct node *temp; // local variable temp
 	temp=(struct node*)malloc(sizeof(struct node));
     printf("enter data for the node:\n");
 	scanf("%d",&temp->data);
 	temp->link=NULL;
-	if(root==NULL)
+	if(root==NULL) // check weather other nodes are present or not 
 	{
 		root=temp;
 	}
+	else
+	{
 	temp->link=root;
 	root=temp;
+	}
 }
 
 int len()
@@ -75,4 +76,19 @@ int len()
 	}
 	//printf("count:%d\n",count);
 	return count;
+}
+
+void display()
+{
+	struct node *temp; // local variable temp
+	int count=0;
+	    printf("root:%d\n",root);
+		printf("nodes data:\n");
+		temp=root;
+		while(temp!=NULL)
+		{
+			count++;
+			printf("node %d data:%d link address:%u\n",count,temp->data,temp->link);
+			temp=temp->link;
+		}
 }
